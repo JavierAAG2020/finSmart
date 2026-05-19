154483e3b693299206989eb8336c1f6d4805ba14
Santiag27 <sotodavid405@gmail.com>
2026-05-18 20:31:02 -0500

    Subida y bajada de archivos

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const txRoutes = require('./routes/transactions')
 const aiRoutes = require('./routes/ai')
+const filesRoutes = require('./routes/files'); // Importa el router "files.js"
+const path = require('path'); // Usado para rutas estáticas
 require('dotenv').config()
 require('./db') // pool
 
@@ -11,8 +13,12 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/files', filesRoutes)
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')))
 app.use('/api', txRoutes)
 app.use('/api/ai', aiRoutes)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`API server listening on http://localhost:${PORT}`))
+
+
--- frontend/routes/AppRouter.jsx
+++ frontend/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home"
 import Dashboard from "../pages/Dashboard"
 import Contact from "../pages/Contact"
 import ProtectedRoute from "../components/auth/ProtectedRoute"
+import Files from "../pages/Files"
 
 function AppRouter() {
   return (
@@ -10,7 +11,7 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-
+        <Route path="/files" element={<Files />} />
         <Route
           path="/dashboard"
           element={
